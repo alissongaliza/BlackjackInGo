4fd35bf09ac52271510b5d8361380501594bf484
Alisson Galiza <alissongaliza@gmail.com>
2020-04-16 19:10:20 -0300

    new games route

--- main.go
+++ main.go
@@ -5,30 +5,33 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/alissongaliza/BlackjackInGo/game"
 	"github.com/alissongaliza/BlackjackInGo/user"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
-func pathPrinter(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Do stuff here
-		log.Println(r.RequestURI)
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
-		next.ServeHTTP(w, r)
-	})
-}
-
 func pingLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Pong")
 }
 
+// func initInMemoryDataStore() map[Actors]interface{} {
+
+// 	// defer models.Sess.Close()
+// 	// models.Sess.Exec(models.)
+// 	db = models.GetDb()
+// 	return db
+// }
+
 func main() {
+	// db := initInMemoryDataStore()
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", pingLink)
 	r.Mount("/users", user.UserRouter())
+	r.Mount("/games", game.GameRouter())
 	// r.Mount(game.gameRoutes)
 
 	fmt.Println("Listening")
