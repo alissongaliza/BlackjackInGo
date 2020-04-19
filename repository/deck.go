834b5ce0b017c54323a322ed93a3e27d8269ed5c
Alisson Galiza <alissongaliza@gmail.com>
2020-04-18 23:20:16 -0300

    Implementing Hand and Cards further

--- repository/deck.go
+++ repository/deck.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"strconv"
+)
+
 type SuitType string
 
 const (
@@ -10,11 +14,31 @@ const (
 )
 
 type Card struct {
-	suit SuitType
-	name string
+	Suit     SuitType
+	Name     string
+	isNumber bool
 }
 
 type Hand struct {
-	cards []*Card
-	score int
+	Cards []Card
+	Score int
+}
+
+func (card Card) value(hand Hand) int {
+	if card.isNumber {
+		// convert from string to int
+		if s, err := strconv.Atoi(card.Name); err == nil {
+			return s
+		}
+	}
+
+	if card.Name == "ace" {
+		if hand.Score+11 <= 21 {
+			return 11
+		}
+		return 1
+
+	}
+
+	return 10
 }
