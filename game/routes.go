d9ea692f3ba4981ffb925904404f9ebfa28131f3
Alisson Galiza <alissongaliza@gmail.com>
2020-04-20 18:02:42 -0300

    Treating errors with panic/recover

--- game/routes.go
+++ game/routes.go
@@ -17,12 +17,12 @@ func findGame(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "finding games...")
 }
 
-func createGame(playerId int, dif models.Difficuty, w http.ResponseWriter) models.Game {
+func createGame(playerId int, dif models.Difficuty, bet int, w http.ResponseWriter) models.Game {
 	fmt.Println("creating game...", playerId, dif)
 	if !models.IsPlayerValid(playerId) {
-		fmt.Fprintf(w, "Player of id %d not in the database.", playerId)
+		panic(fmt.Sprintf("Player of id %d not in the database.", playerId))
 	}
-	game := models.NewGame(playerId, dif)
+	game := models.NewGame(playerId, dif, bet)
 	return game
 }
 
@@ -30,10 +30,19 @@ func startGame(w http.ResponseWriter, r *http.Request) {
 	var data struct {
 		Id  int
 		Dif models.Difficuty
+		Bet int
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			// fmt.Fprint(w, r)
+			fmt.Println(r)
+		}
+	}()
 	json.NewDecoder(r.Body).Decode(&data)
-	newGame := createGame(data.Id, data.Dif, w)
-	json.NewEncoder(w).Encode(newGame.Id)
+	newGame := createGame(data.Id, data.Dif, data.Bet, w)
+	fmt.Println("Game Created")
+	game := models.StartGame(&newGame)
+	json.NewEncoder(w).Encode(game)
 }
 
 func GameRouter() chi.Router {
