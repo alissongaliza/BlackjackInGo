a0128c7712f5f158e54c4ca065dbb1385dfad98d
Alisson Galiza <alissongaliza@gmail.com>
2020-04-21 16:05:43 -0300

    New utils function to convert strings to int

--- utils/numbers.go
+++ utils/numbers.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -9,4 +11,14 @@ func GetRandomNumber(start int, end int) (number int) {
 	rand.Seed(time.Now().UnixNano())
 	number = rand.Intn(end-start+1) + start
 	return
+}
+
+func StringToInt(str string) int {
+
+	if s, err := strconv.Atoi(str); err == nil {
+		return s
+	} else {
+		fmt.Println(err)
+		return -1
+	}
 }
