9e465a05092686aee589f8db757a3681b2871ce4
Alisson Galiza <alissongaliza@gmail.com>
2020-04-26 00:01:55 -0300

    Returning the game for convenience

--- repository/user.go
+++ repository/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User interface {
-	Hit(gameId int, faceUp bool)
-	Stand(gameId int)
-	DoubleDown(gameId int)
+	Hit(gameId int, faceUp bool) (game Game)
+	Stand(gameId int) (game Game)
+	DoubleDown(gameId int) (game Game)
 }
