d304c6452512b6bae4a6b054e656f7921cfeb28d
Alisson Galiza <alissongaliza@gmail.com>
2020-04-27 00:01:50 -0300

    Dealers only implements Play() method now

--- repository/dealer.go
+++ repository/dealer.go
@@ -9,12 +9,11 @@ import (
 type Difficuty string
 
 const (
-	easy Difficuty = "easy"
-	hard Difficuty = "hard"
+	easy   Difficuty = "easy"
+	broken Difficuty = "broken"
 )
 
 type Dealer interface {
-	Player
 	Play(Game) Game
 }
 
@@ -24,22 +23,34 @@ type EasyDealer struct {
 	Hand      *Hand
 }
 
-type HardDealer struct {
+type BrokenDealer struct {
 	Name      string
 	Difficuty Difficuty
 	Hand      *Hand
 }
 
 func (easy *EasyDealer) Play(currentGame Game) Game {
+	if currentGame.GameState != playing {
+		return currentGame
+	}
 	if easy.Hand.Score <= 17 {
-		return easy.Hit(currentGame.Id, true)
+		return Hit(currentGame.Id, true)
 	} else {
-		return easy.Stand(currentGame.Id)
+		return Stand(currentGame.Id)
 	}
 }
 
-func (easy *EasyDealer) Hit(gameId int, faceUp bool) (game Game) {
-	fmt.Println("EasyDealer hit reached")
+func (broken *BrokenDealer) Play(currentGame Game) Game {
+	if broken.Hand.Score <= 17 &&
+		currentGame.User.Hand.Score > currentGame.User.Hand.Score {
+		return Hit(currentGame.Id, true)
+	} else {
+		return Stand(currentGame.Id)
+	}
+}
+
+func Hit(gameId int, faceUp bool) (game Game) {
+	fmt.Println("Dealer hit reached")
 	game = GetGameDb().Get(gameId)
 	//pop an element from the cards array
 	index := utils.GetRandomNumber(0, len(game.Cards)-1)
@@ -47,36 +58,31 @@ func (easy *EasyDealer) Hit(gameId int, faceUp bool) (game Game) {
 	game.Cards[index] = game.Cards[len(game.Cards)-1]
 	game.Cards = game.Cards[:len(game.Cards)-1]
 
-	if faceUp {
-		card.isFaceUp = true
-	}
+	card.isFaceUp = faceUp
 	// assign the new cards to the dealers's hand
 	dealer := game.Dealer.(*EasyDealer)
 	hand := dealer.Hand
+	if hand.Score+card.value(*hand) > 21 {
+		game.GameState = lost
+	}
 	hand.Cards = append(hand.Cards, card)
 	hand.Score += card.value(*hand)
 	// finish turn
 	game.isUserTurn = true
-	fmt.Println(dealer.Hand)
+	game.LastDealerAction = hit
+	fmt.Println("dealer", dealer.Hand, game.GameState)
 	GetGameDb().Update(game)
 	return
 }
 
-func (easy EasyDealer) Stand(gameId int) (game Game) {
+func Stand(gameId int) (game Game) {
 	fmt.Println("Dealer stands!")
 	game = GetGameDb().Get(gameId)
 	game.LastDealerAction = stand
 	game.isUserTurn = true
+	game.LastDealerAction = stand
+	dealer := game.Dealer.(*EasyDealer)
+	fmt.Println("dealer", dealer.Hand, game.GameState)
 	GetGameDb().Update(game)
-	return
-}
-
-func (hard *HardDealer) Hit(gameId int, faceUp bool) (game Game) {
-	fmt.Println("HardDealer hit!")
-	return
-}
-
-func (hard *HardDealer) Stand(gameId int) (game Game) {
-	fmt.Println("HardDealer stand!")
 	return
 }
