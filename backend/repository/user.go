8f1adced07d1bb5670b85f42a752c723a4d4c6e7
Alisson Galiza <alissongaliza@gmail.com>
2020-05-03 17:49:50 -0300

    chore

--- backend/repository/user.go
+++ backend/repository/user.go
@@ -15,7 +15,6 @@ func NewUser(name string, age int) (newUser User) {
 func (user User) Hit(gameId int, faceUp bool) (game Game) {
 	fmt.Printf("user %d hits!", user.Id)
 	game = GetGameDb().Get(gameId)
-	fmt.Println(game.GameState)
 	if game.GameState != utils.Playing {
 		panic(fmt.Sprintf("Game is already over. User %s", game.GameState))
 	}
@@ -28,17 +27,16 @@ func (user User) Hit(gameId int, faceUp bool) (game Game) {
 	card.IsFaceUp = faceUp
 	// assign the new cards to the user's hand
 	hand := game.User.Hand
-	if hand.Score+card.value(*hand) > 21 {
-		game.GameState = utils.Lost
-	}
 	hand.Cards = append(hand.Cards, card)
 	if card.IsFaceUp {
 		hand.Score += card.value(*hand)
 	}
 	fmt.Println("user", game.User.Hand, game.GameState)
-	//call dealers turn
+	// check if burst happened
+	if hand.Score > 21 {
+		game.GameState = utils.Lost
+	}
 	GetGameDb().Update(game)
-	game = game.Dealer.DealerActions.Play(game)
 	return
 }
 
@@ -46,8 +44,8 @@ func (user User) Stand(gameId int) (game Game) {
 	fmt.Printf("user %d stands!", user.Id)
 	game = GetGameDb().Get(gameId)
 	game.LastUserAction = utils.Stand
-	//call dealers turn
 	GetGameDb().Update(game)
+	//call dealers turn
 	game = game.Dealer.Play(game)
 	return
 }
@@ -57,7 +55,7 @@ func (user User) DoubleDown(gameId int) (game Game) {
 	game = GetGameDb().Get(gameId)
 	if len(game.User.Hand.Cards) != 2 {
 		panic(`User can't Double Down. This move is only 
-		available when he has only the first 2 starting cards`)
+		available when he only has the first 2 starting cards`)
 	} else if game.User.Chips < game.Bet {
 		panic(`User can't Double Down. His current chips balance is lower than necessary`)
 	}
