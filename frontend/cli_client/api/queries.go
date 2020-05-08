48d10f6f65f3f27bc9ab6060b4064b237fe72d4c
Alisson Galiza <alissongaliza@gmail.com>
2020-05-07 22:32:27 -0300

    New endpoint access FindOngoingGames

--- frontend/cli_client/api/queries.go
+++ frontend/cli_client/api/queries.go
@@ -6,16 +6,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/alissongaliza/BlackjackInGo/cliClient/utils"
 	remoteUtils "github.com/alissongaliza/BlackjackInGo/utils"
 )
 
-func CreateUser(user utils.User) {
+func CreateUser(age int, name string) (newUser utils.User) {
 
+	userRequest := utils.NewUserRequest{Age: age, Name: name}
 	buf := new(bytes.Buffer)
-	json.NewEncoder(buf).Encode(user)
-	req, err := http.NewRequest("POST", "http://localhost:8080/user", buf)
+	json.NewEncoder(buf).Encode(&userRequest)
+	req, err := http.NewRequest("POST", "http://localhost:8080/users", buf)
 	if err != nil {
 		log.Print(err)
 	}
@@ -25,11 +27,14 @@ func CreateUser(user utils.User) {
 	if e != nil {
 		log.Print(e)
 	}
-
 	defer res.Body.Close()
+
+	json.NewDecoder(res.Body).Decode(&newUser)
+	fmt.Println(newUser)
+	return
 }
 
-func FindUser(username string) (users []utils.User) {
+func FindUser(username string) utils.User {
 
 	buf := new(bytes.Buffer)
 	json.NewEncoder(buf).Encode(username)
@@ -48,9 +53,40 @@ func FindUser(username string) (users []utils.User) {
 	}
 
 	defer res.Body.Close()
+	var users []utils.User
 	json.NewDecoder(res.Body).Decode(&users)
 
-	return users
+	if len(users) > 0 {
+		return users[0]
+	} else {
+		panic("No user found with this username")
+	}
+}
+
+func FindOngoingGamesOfUser(userId int) (games []utils.Game) {
+	fmt.Println("id", userId, string(userId))
+
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(&userId)
+	req, err := http.NewRequest("GET", "http://localhost:8080/games", buf)
+	q := req.URL.Query()
+	q.Add("userId", strconv.Itoa(userId))
+	req.URL.RawQuery = q.Encode()
+	if err != nil {
+		log.Print(err)
+	}
+
+	client := &http.Client{}
+	res, e := client.Do(req)
+	if e != nil {
+		log.Print(e)
+	}
+
+	defer res.Body.Close()
+	json.NewDecoder(res.Body).Decode(&games)
+	fmt.Println("games", games)
+
+	return games
 }
 
 func StartGame(userId int, dif remoteUtils.Difficulty, bet int) (game utils.Game) {
